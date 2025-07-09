e44cedda5c57d80f0f56e6de2e3fe7d35f9b0a08
mhwx2333 <3108483225@qq.com>
2025-07-09 22:25:43 +0800

    Delete yunstroe-hd directory

--- yunstroe-hd/src/main/java/com/mh/yunstroehd/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.mh.yunstroehd.service;
-
-import com.mh.yunstroehd.entity.Menu;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-
-public interface MenuService extends IService<Menu> {
-
-}
